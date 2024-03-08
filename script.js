6370cc0abd898d6dbfa6f323cdffa78498191ac4
yecrystal <123497862+yecrystal@users.noreply.github.com>
2024-03-07 22:55:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["STUDENT.", "SOFTWARE ENGINEER.", "SOFTWARE DEVELOPER"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1000,
+    loop: true
+});
+
